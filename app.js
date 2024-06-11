30940c9753dda973a2ccafdf888067f59efff8e6
Aneesh Hegde <aneeshhegde7110@gmail.com>
2024-06-11 23:32:55 +0530

    added user functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,6 +8,14 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const bodyParser = require('body-parser');
 const Padai = require("./schema/padaiSchema");
+const User = require("./schema/user");
+const passport = require("passport");
+const LocalStratergy = require("passport-local");
+const MongoStore=require("connect-mongo");
+const session = require("express-session");
+
+
+
 
 app.listen(8080, () => {
     console.log("app is listening on port 8080");
@@ -23,6 +33,37 @@ app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//session parameter
+const store=MongoStore.create({
+    mongoUrl: "mongodb://127.0.0.1:27017/habittracker",
+    crypto: {
+      secret: process.env.SECRET,
+    },
+    touchAfter: 24*3600,
+  });
+//express session
+const sessionOptions = {
+    store,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly:true,
+    }
+  };
+  
+  
+  app.use(session(sessionOptions));
+//password initialization
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratergy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 // Connect to MongoDB
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/habittracker");
@@ -36,7 +77,8 @@ main()
 
 // Route handlers
 app.get("/", async (req, res) => {
-    let padaiTask = await Padai.find({});
+    let user=await User.findById(req.session.user).populate("padai");
+    let padaiTask = user.padai;
     res.render("index.ejs", { padaiTask });
 });
 
@@ -51,6 +93,9 @@ app.post("/padai", async(req, res) => {
     let newTask=new Padai(data);
     console.log(newTask);
     await newTask.save();
+    let user=await User.findById(req.session.user);
+    user.padai.push(newTask);
+    await User.findByIdAndUpdate(req.session.user,user)
     res.redirect("/");
 });
 
@@ -78,6 +123,34 @@ app.delete("/padai/:id",async(req,res)=>{
     res.redirect('/');
 })
 
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs");
+});
+app.post("/login",passport.authenticate("local",{failureRedirect:"/login"}),async(req,res)=>{
+    let user=await User.findById(req.user._id);
+    req.session.user=req.user._id;
+    console.log(req.session.user);
+    console.log(user);
+    res.redirect("/");
+})
+
+app.get("/signup",(req,res)=>{
+    res.render("signup.ejs")
+})
+
+app.post("/signup",async(req,res)=>{
+    try{
+        let  {username,password}=req.body;
+        let newUser=new User({username,password});
+        let registeredUser=await User.register(newUser,password);
+        console.log(registeredUser);
+        res.redirect("/");
+    }catch(err){
+        console.log(err);
+        res.redirect("/signup");
+    }
+})
 
 // const express = require("express");
 // const app = express();
